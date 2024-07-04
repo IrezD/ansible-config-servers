59caecad86e4f3599a6d7356e602570c0c9aebe6
Dennis Owie <dennysben@gmail.com>
2024-07-04 12:49:18 +0200

    function added to stop or stop server

--- server_script.py
+++ server_script.py
@@ -8,10 +8,11 @@ ec2 = boto3.client("ec2")
 SECURITY_GROUP_ID = os.getenv("SECURITY_GROUP_ID")
 INSTANCE_TYPE = os.getenv("INSTANCE_TYPE")
 KEYNAME = os.getenv("KEYNAME")
+INSTANCE_ID = os.getenv("INSTANCE_ID")
 
 COUNT = 1
 
-# create ansible server (Ubuntu)
+# ** Create Ansible Server (Ubuntu)
 
 def create_server():
     server = ec2.run_instances(
@@ -37,7 +38,7 @@ def create_server():
     print (server)
     
     
-# describes the server
+# ** Describes The Server
 
 def describe_server():
     response = ec2.describe_instances(
@@ -47,8 +48,30 @@ def describe_server():
     METADATA = response['Reservations'][0]['Instances'][0]
 
     INSTANCE_STATE = METADATA['State']['Name']
-    PUBLIC_IP = METADATA['PublicIpAddress']
+    PUBLIC_IP = METADATA['PublicIpAddress']    
     
     print(f"Instance is {INSTANCE_STATE}. \nPublic IP: {PUBLIC_IP} ")
 
-describe_server()
+
+# ** Start Server
+
+def start_server():
+    ec2.start_instances(
+        InstanceIds = [INSTANCE_ID]
+    )
+
+    print(f"Server - {INSTANCE_ID} is now Running.")
+
+# ** Stop Server
+
+def stop_server():
+    ec2.stop_instances(
+        InstanceIds = [INSTANCE_ID]
+    )
+
+    print(f"Server - {INSTANCE_ID} has been Stopped.")
+
+
+# ** Call Any Function to Run
+
+start_server()
